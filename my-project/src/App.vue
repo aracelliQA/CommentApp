c30fb2f76475d123b268304e5b366a2e11e844c6
aracelliQA <aracellieng@gmail.com>
2023-03-14 22:53:37 -0300

    Comment App

---
+++ my-project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Comment/>
+</template>
+
+<script>
+import Comment from './components/Comment.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Comment
+  }
+}
+</script>
+
+<style>
+
+</style>
